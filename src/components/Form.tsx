253223827dc8303bc92652135a44995d99392a25
Caranell <lyachek.anton@gmail.com>
2019-08-12 20:09:53 +0500

    Finished basic logic

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,7 +7,7 @@ interface State {
   email: string;
   category: string;
   message: string;
-  img: '';
+  img: File | null;
 }
 class FormControl extends React.Component<{}, State> {
   constructor() {
@@ -18,25 +18,70 @@ class FormControl extends React.Component<{}, State> {
       email: '',
       category: '',
       message: '',
-      img: '',
+      img: null,
     };
   }
-  private onFieldInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+  private onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name: field, value } = event.target;
-    console.log(field);
     this.setState({
       ...this.state,
       [field]: value,
     });
+  };
+  private onSelectChnage = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+    this.setState({
+      ...this.state,
+      category: value,
+    });
     console.log(this.state);
   };
-  private onSend = () => {};
-  public render() {
+  private onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      const fileSize = file.size / 1024 / 1024;
+      if (fileSize < 2 && (file.type === 'image/jpeg' || file.type === 'image/png')) {
+        this.setState({
+          ...this.state,
+          img: file,
+        });
+      } else {
+        this.setState({
+          ...this.state,
+          img: null,
+        });
+      }
+    }
+  };
+  private onSend = () => {
     const { firstName, secondName, email, category, message, img } = this.state;
+    if (email && (firstName || secondName) && category && message.length > 9 && img) {
+      //create json object
+    }
+  };
+
+  private onFileInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = new FormData();
+  };
+  public render() {
     return (
       <form action="">
         <label htmlFor="">Name</label>
-        <input type="text" name="firstName" onInput={this.onFieldInput} />
+        <input type="text" name="firstName" onInput={this.onInputChange} />
+        <label htmlFor="">SecondName</label>
+        <input type="text" name="secondName" onInput={this.onInputChange} />
+        <label htmlFor="">Email</label>
+        <input type="email" name="email" onInput={this.onInputChange} />
+        <label htmlFor="">Message</label>
+        <input type="text" name="message" onInput={this.onInputChange} />
+        <select name="category" id="" onChange={this.onSelectChnage}>
+          <option value="" selected hidden>
+            Выберите тип
+          </option>
+          <option value="Жалоба">Жалоба</option>
+          <option value="Благодарность">Благодарность</option>
+        </select>
+        <input type="file" name="image" onChange={this.onImageChange} />
       </form>
     );
   }
