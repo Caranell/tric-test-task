9e0e9bf3326af41f31e2a00be25e220b1a42420f
Caranell <lyachek.anton@gmail.com>
2019-08-14 00:49:26 +0500

    Updates

---
+++ precache-manifest.7a10c5d7e587e0162fc2f70aea6699d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96132703afedcd1096a9f05d1ad9e18a",
+    "url": "/tric-test-task/index.html"
+  },
+  {
+    "revision": "13a3513a21c1469f4905",
+    "url": "/tric-test-task/static/css/main.e969f607.chunk.css"
+  },
+  {
+    "revision": "46622f4ebfcb94495203",
+    "url": "/tric-test-task/static/js/2.2f9ff574.chunk.js"
+  },
+  {
+    "revision": "13a3513a21c1469f4905",
+    "url": "/tric-test-task/static/js/main.876d393f.chunk.js"
+  },
+  {
+    "revision": "b379bc9064038af1a6d7",
+    "url": "/tric-test-task/static/js/runtime~main.015826b4.js"
+  }
+]);
